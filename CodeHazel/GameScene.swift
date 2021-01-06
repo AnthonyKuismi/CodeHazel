700e53883692e385dd9f59e421aa034953823551
AnthonyKuismi <61592276+AnthonyKuismi@users.noreply.github.com>
2021-01-06 09:49:43 -0600

    fixed

--- CodeHazel/GameScene.swift
+++ CodeHazel/GameScene.swift
@@ -10,52 +10,5 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    var startText = SKLabelNode(fontNamed: "AvenirNext-Bold")
-    
-    override func didMove(to view: SKView) {
-        setup()
-    }
-    
-    func setup(){
-        setupStartScreen()
-        setupNames()
-    }
-    
-    func setupNames(){
-        startText.name = "startText"
-    }
-    
-    func setupStartScreen(){
-        removeAllChildren()
-        startText.fontSize = 30
-        startText.fontColor = UIColor.green
-        startText.text = "Place Holder"
-        addChild(startText)
-    }
-    
-    func setupGameScreen(){
-        removeAllChildren()
-        startText.fontColor = UIColor.red
-        addChild(startText)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        buttonPress(touch: touches.first!)
-    }
-    
-    func buttonPress(touch: UITouch){
-        enumerateChildNodes(withName: "//*") { [self] (node, stop) in
-            let location = touch.location(in: self)
-            if node.name == "startText"{
-                if (self.startText.contains(location)){
-                    setupGameScreen()
-                }
-            }
-        }
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+  
 }
